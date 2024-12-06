61901cee4f0486c66eacfd43cecf988ad23360d9
Твоё Имя < Email  GitHub>
2024-12-06 13:47:01 +0100

    Commit: 2024-12-06 13:47:00

---
+++ Lesson_11_05_12_24/Homework/script.js
@@ -0,0 +1,15 @@
+// 1. Деление числа 100 на остаток от деления 25 на 3
+let myNumber = 100 / (25 % 3);
+console.log(myNumber);
+
+// 2. Сложение строк и числа в одну строку
+let myString = "Hello, " + "world" + "!" + 2024;
+console.log(myString);
+
+// 3. Сравнение числа 42 со строкой "42" с результатом true
+let isHuman = 42 == "42";
+console.log(isHuman);
+
+// 4. Преобразование строки "123smth" в число
+myNumber = parseInt("123smth");
+console.log(myNumber);

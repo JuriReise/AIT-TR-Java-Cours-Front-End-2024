3135cbdc4c8329ed39b4b613f531d02c4e56d890
Твоё Имя < Email  GitHub>
2024-12-18 13:02:17 +0100

    Commit: 2024-12-18 13:02:16

--- Lesson_20_18_12_24_fetchWeatherGeoData/Classwork/scrypt.js
+++ Lesson_20_18_12_24_fetchWeatherGeoData/Classwork/scrypt.js
@@ -1,7 +1,15 @@
+// лоадер
+function manageLoader(show) {
+    const loader = document.querySelector('.loader');
+    loader.style.display = show ? 'inline-block' : 'none';
+}
+
 // геоданные
 async function fetchGeoData() {
     const geoUrl = 'https://get.geojs.io/v1/ip/geo.json';
     const geoDataElement = document.getElementById('geo-data');
+
+    manageLoader(true); 
 
     try {
         
@@ -15,18 +23,26 @@ async function fetchGeoData() {
             <p>Город: ${city}</p>
         `;
 
+        // Добавляем искусственную задержку для демонстрации загрузчика
+        await new Promise(resolve => setTimeout(resolve, 5000));
+
         
         await fetchWeatherData(latitude, longitude, city);
     } catch (error) {
         console.error('Ошибка при получении геоданных:', error.message);
         geoDataElement.textContent = 'Не удалось загрузить данные о вашем местоположении.';
-    }
+    } finally {
+        manageLoader(false); 
+
+    } 
 }
 
 // погода
 async function fetchWeatherData(latitude, longitude, city) {
     const weatherUrl = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true&precipitation_probability=true`;
     const weatherDataElement = document.getElementById('weather-data');
+
+    manageLoader(true);
 
     try {
         
@@ -48,18 +64,22 @@ async function fetchWeatherData(latitude, longitude, city) {
     } catch (error) {
         console.error('Ошибка при получении данных о погоде:', error.message);
         weatherDataElement.textContent = 'Не удалось загрузить данные о погоде.';
+
+    } finally {
+        manageLoader(false);
+
     }
 }
 
 // виды погоды
 function decodeWeatherCode(code) {
     const weatherCodes = {
-        0: 'Ясно',
-        1: 'В основном ясно',
-        2: 'Переменная облачность',
-        3: 'Пасмурно',
-        45: 'Туман',
-        48: 'Осаждающий туман',
+        0: 'Ясно☀️',
+        1: 'В основном ясно⛅',
+        2: 'Переменная облачность⛅',
+        3: 'Пасмурно🙁',
+        45: 'Туман🌁',
+        48: 'Осаждающий туман🌫️:',
         51: 'Лёгкая морось',
         53: 'Умеренная морось',
         55: 'Сильная морось',
@@ -68,10 +88,10 @@ function decodeWeatherCode(code) {
         65: 'Сильный дождь',
         71: 'Лёгкий снег',
         73: 'Умеренный снег',
-        75: 'Сильный снег',
+        75: 'Сильный снег❄️',
         80: 'Лёгкий ливень',
         81: 'Умеренный ливень',
-        82: 'Сильный ливень',
+        82: 'Сильный ливень🌨️',
     };
 
     return weatherCodes[code] || 'Неизвестная погода';
